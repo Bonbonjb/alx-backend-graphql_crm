8361123a9771a72ecdfa8a8e91ca98b50c4eb898
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-07-11 14:00:40 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,11 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 
-from django.contrib import admin
-from django.urls import path
-from graphene_django.views import GraphQLView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
