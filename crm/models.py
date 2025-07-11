90df2d207499df4527711e433a8ab955db0faa1b
Brenda Bonareri <bonarerijb24@gmail.com>
2025-07-11 15:06:24 +0300

    updated models.py

--- crm/models.py
+++ crm/models.py
@@ -8,7 +8,16 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
-
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     order_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer.name}"
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
