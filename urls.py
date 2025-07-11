f27c79a0e0a09faa0caf77e40969b59f3c4407c7
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-07-11 14:04:17 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
-
 from django.contrib import admin
-from django.urls import path
-from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
