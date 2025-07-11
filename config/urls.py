a8d5d3c79c825e46fc22f23e7febb46b8c996d81
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-07-11 14:50:34 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.views.decorators.csrf import csrf_exempt
 from crm.schema import schema
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path("admin/", admin.site.urls),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(schema=schema))),
 ]
