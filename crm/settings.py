fe3920712904da0035b6df120bad76a171e6d929
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-07-15 00:42:43 +0300

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -123,6 +123,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRONJOBS = [
-    ('0 */12 * * *', 'crm.cron.update_low_stock'),
-    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),  
+    ('0 */12 * * *', 'crm.cron.update_low_stock')  
 ]
