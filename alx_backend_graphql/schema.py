6abcfbb1ed011f566366355677b6f3e840eff06f
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-07-24 13:31:47 +0300

    Update schema.py

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from crm.schema isport Query as CRMQuery, Mutation as CRMMutation
+from crm.schema import Query
 
 class Query(CRMQuery, graphene.ObjectType):
     hello graphene.String()
