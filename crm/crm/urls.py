1ebcf79efcff87bebb34858e27f059d61a258032
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-07-25 15:15:52 +0300

    Update urls.py

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for crm project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+from crm.schema import schema
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(schema=schema))),
 ]
